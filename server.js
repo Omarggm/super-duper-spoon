aae6868cc4d91ce97eff6c8e948c3fc021c8c74c
Omarggm <123112227+Omarggm@users.noreply.github.com>
2023-06-21 07:40:03 -0700

    functions to interact with depts emplyee roles

--- server.js
+++ server.js
@@ -80,3 +80,107 @@ async function startApp() {
     );
 }
 
+function viewDepartments() {
+    connection.query('SELECT * FROM department', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
+
+function viewRoles() {
+    connection.query('SELECT * FROM role', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
+
+function viewEmployees() {
+    connection.query('SELECT * FROM employee', function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startApp();
+    });
+}
+
+function addDepartment() {
+    inquirer
+    .prompt([
+        {
+            name: 'department',
+            type: 'input',
+            message: 'What is the name of the department?',
+        },
+    ])
+    .then((answer) => {
+        connection.query(
+            'INSERT INTO department SET ?',
+            {
+                name: answer.department,
+            },
+            function (err, res) {
+                if (err) throw err;
+                console.log('Department added!');
+                startApp();
+            }
+        );
+    });
+}
+
+function addRole() {
+    inquirer
+    .prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'Enter the emnployee\'s first name.',
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'Enter the employee\'s last name.',
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message: 'Select the employee\'s role.',
+            choices: roles.map((role) => role.title),
+        },
+        {
+            name: 'manager',
+            type: 'list',
+            message: 'Enter the employee\'s manager.',
+            choices: [
+                'None',
+                ...employees.map(employee => `${employee.first_name} ${employee.last_name}`),
+
+            ],
+        },
+    ])
+    .then((answer) => {
+        const role = roles.find((role) => role.title === answer.role);
+        const managerName = answer.manager.split(' ');
+        let managerId = null;
+        if (answer.manager !== 'None') {
+            const manager = employees.find(
+                (employee) => employee.first_name === managerName[0] && employee.last_name === managerName[1]
+            );
+            managerId = manager.id;
+        }
+        connection.query(
+            'INSERT INTO employee SET ?',
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: role.id,
+                manager_id: managerId,
+            },
+            function (err, res) {
+                if (err) throw err;
+                console.log('Employee added succesfully!');
+                startApp();
+            }
+        );
+    });
+}
